948ac4d916db1f72598597790bdfab6d37a17ba3
HeroxXd <96165764+HeroxXd@users.noreply.github.com>
2021-12-15 19:26:40 +0530

    Update utils.py

--- senorita/utils.py
+++ senorita/utils.py
@@ -1,6 +1,6 @@
 import os
 import asyncio
-from senorita import bot, call_py
+from senoritacall import bot, call_py
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
 from senorita.queues import QUEUE, clear_queue, get_queue, pop_an_item
